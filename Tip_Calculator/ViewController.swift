e98a8d7ca0783bbaf9b1bd6ab6794792a2ede68c
blaineconnor <112892044+blaineconnor@users.noreply.github.com>
2024-03-04 08:30:47 +0300

    Initial Commit

---
+++ Tip_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tip_Calculator
+//
+//  Created by Fatih Emre Sarman on 4.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
