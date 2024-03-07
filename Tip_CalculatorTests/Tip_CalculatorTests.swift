11fb432e94f1960cae32b3161be41eda6859c712
blaineconnor <112892044+blaineconnor@users.noreply.github.com>
2024-03-07 14:13:06 +0300

    Setup Unit Tests Successful.

--- Tip_CalculatorTests/Tip_CalculatorTests.swift
+++ Tip_CalculatorTests/Tip_CalculatorTests.swift
@@ -6,31 +6,130 @@
 //
 
 import XCTest
+import Combine
 @testable import Tip_Calculator
 
 final class Tip_CalculatorTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    //sut -> System Under Test
+    private var sut: CalculatorVM!
+    private var cancellable: Set<AnyCancellable>!
+    private var logoViewTapSub: PassthroughSubject<Void,Never>!
+    private var audioPlayerService: MockAudioPlayerService!
+    
+    override func setUp() {
+        audioPlayerService = .init()
+        sut = .init(audioPlayerService: audioPlayerService)
+        logoViewTapSub = .init()
+        cancellable = .init()
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        sut = nil
+        cancellable = nil
+        audioPlayerService = nil
+        logoViewTapSub = nil
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    //EN: One person without tip --- TR: Bahşiş vermeden tek kişilik hesap
+    func testResultWithoutTipFor1Person(){
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 1
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amounPerPerson, 100)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellable)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    // - $100 bill
+    // - no tip
+    // - 1 person
+    
+    //EN: Two people without tip --- TR: Bahşiş vermeden iki kişilik hesap
+    func testResultWithoutTipFor2People(){
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amounPerPerson, 50)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellable)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    //EN: Two people %10 tip --- TR: %10 bahşişle iki kişilik hesap
+    func testResultWith10PercentTipFor2People(){
+        // given
+        let bill: Double = 100.0
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amounPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellable)
     }
+    
+    //EN: Four people custom tip --- TR: Özel bahşiş dört kişilik hesap
+    func testResultWithCustomtipFor4People(){
+        // given
+        let bill: Double = 200.0
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        //when
+        let output = sut.transform(input: input)
+        //then
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amounPerPerson, 100.25)
+            XCTAssertEqual(result.totalBill, 401)
+            XCTAssertEqual(result.totalTip, 201)
+        }.store(in: &cancellable)
+    }
+    
+    //EN: Calculator reset and sound test when logo tapped--- TR: Logoya tıklandığında hesap sıfırlama ve ses çıkışı
+    func testSoundPlayedAndCalculatorResetOnLogoViewTap(){
+        //given
+        let input = buildInput(bill: 100, tip: .tenPercent, split: 2)
+        let output = sut.transform(input: input)
+       let expectation1 = XCTestExpectation(description: "reset calculator called")
+        let expectation2 = audioPlayerService.expectation
+        //then
+        output.resetCalculatorPublisher.sink { _ in
+            expectation1.fulfill()
+        }.store(in: &cancellable)
+        
+        //when
+        logoViewTapSub.send()
+        wait(for: [expectation1, expectation2], timeout: 1.0)
+    }
+    
+    private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.Input {
+        return .init(billPublisher: Just(bill).eraseToAnyPublisher(), tipPublisher: Just(tip).eraseToAnyPublisher(), splitPublisher: Just(split).eraseToAnyPublisher(), logoViewTapPublisher: logoViewTapSub.eraseToAnyPublisher())
+    }
+}
 
+class MockAudioPlayerService: AudioPlayerService {
+    var expectation = XCTestExpectation(description: "playSound is called")
+    func playSound() {
+        expectation.fulfill()
+    }
 }
